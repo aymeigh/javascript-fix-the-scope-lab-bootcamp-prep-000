202cfc918ea65e3164d25498e7e84c58e13c06fd
Amy Zhou <aymeighz@gmail.com>
2018-06-30 04:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ function funkyFunction (){
 };
 }
 var theFunk = funkyFunction();
-theFunk();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
